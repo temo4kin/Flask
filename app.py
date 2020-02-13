c98090270e31bca032ae400fcc453f8ae9f48550
Artem <temo4kinn@gmail.com>
2020-02-13 22:51:08 +0300

    Обновление главной страницы сайта (случайный список туров)

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import data
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+tours_number = []
+
+for i in range(6):
+    a = random.randint(1, 16)
+    if tours_number.count(a) > 1:
+        a = random.randint(1, 16)
+    tours_number.append(a)
+
+print (tours_number)
+
 @app.route('/')
 def index():
-    index_page = render_template("index.html", title=data.title, subtitle=data.subtitle, description=data.description, departures=data.departures, tours=data.tours)
+    index_page = render_template("index.html", title=data.title, subtitle=data.subtitle, description=data.description, departures=data.departures, tours=data.tours, tours_number=tours_number)
     return index_page
 
 @app.route('/departure/')
